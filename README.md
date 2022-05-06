54c9cc950820569b317cb43c5e33f08c7671f1de
Gosh95 <goshk1995@gmail.com>
2022-05-06 07:50:25 +0400

    init: 프로젝트 생성

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # prgrms-clone-project
+
 프로그래머스 클론 프로젝트
---
+++ src/main/java/prgrms/project/starbooks/StarbooksApplication.java
@@ -0,0 +1,13 @@
+package prgrms.project.starbooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarbooksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarbooksApplication.class, args);
+    }
+
+}
